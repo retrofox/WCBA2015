c653fc200ea1e3bd2a9f43a0a2debd264416cdd0
retrofox <rdsuarez@gmail.com>
2015-05-29 17:00:02 -0300

    updated

--- build.js
+++ build.js
@@ -9,6 +9,12 @@ var oauth = require('wpcom-oauth-cors')('41012', { scope: "global" });
 
 oauth.get(function(token){
   console.log(token);
+
+  // Create wpcom instance given the token
+  var wpcom = WPCOM(token.access_token);
+
+  // expose wpcom globally to test
+  window.wpcom = wpcom;
 });
 
 document.getElementById('reset').onclick = function(e) {
